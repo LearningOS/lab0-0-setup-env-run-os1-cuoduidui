3e6b3f0feec8e5f56f096c573a3f545ef9bae69b
cuoduidui <780106788@qq.com>
2023-01-04 08:24:15 +0000

    1

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,14 +2,12 @@
 // https://zhuanlan.zhihu.com/p/270379116
 #![no_std]
 #![no_main]
-mod lang_items;
 
 #[macro_use]
 mod console;
 mod sbi;
-// fn main() {
-//     // println!("Hello, world!");
-// }
+mod logging;
+mod lang_items;
 
 const SYSCALL_EXIT: usize = 93;
 core::arch::global_asm!(include_str!("entry.asm"));
@@ -37,29 +35,22 @@ const SYSCALL_WRITE: usize = 64;
 pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
   syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
-// #[no_mangle]
-// extern "C" fn _start() {
-//     print!("Hello, ");
-//     println!("world!");
-//     sys_exit(9);
-// }
-
-
  
- #[no_mangle]
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+     }
+     (sbss as usize..ebss as usize).for_each(|a| {
+         unsafe { (a as *mut u8).write_volatile(0) }
+     });
+}
+#[no_mangle]
 pub fn rust_main() -> ! {
     print!("Hello, ");
     println!("world!");
     clear_bss();
     sbi::shutdown();
+   
 }
 
-fn clear_bss() {
-       extern "C" {
-           fn sbss();
-           fn ebss();
-        }
-        (sbss as usize..ebss as usize).for_each(|a| {
-            unsafe { (a as *mut u8).write_volatile(0) }
-        });
-   }
